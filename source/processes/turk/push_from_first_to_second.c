5911af5b2d661e3b7dc20851022edb65e405d2a7
Breno Hildebrand <hello@brenohildebrand.com>
2024-01-25 23:56:38 -0300

    awesome

---
+++ source/processes/turk/push_from_first_to_second.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_from_first_to_second.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bhildebr <bhildebr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/25 23:45:27 by bhildebr          #+#    #+#             */
+/*   Updated: 2024/01/25 23:49:36 by bhildebr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "turk.h"
+
+void	push_from_first_to_second(t_pair stacks, int i, int j)
+{
+	int	possibility;
+
+	possibility = get_possibility(stacks, i, j);
+	if (possibility == 0)
+	{
+		
+	}
+	else if (possibility == 1)
+	{
+		
+	}else if (possibility == 2)
+	{
+		
+	}else if (possibility == 3)
+	{
+		
+	}
+}
