682f50a4ddf1704fd569529cd2d33d63938b7cff
Breno Hildebrand <hello@brenohildebrand.com>
2024-02-14 17:34:24 -0300

    ok

---
+++ submit/lib/ft_framework/includes/ft_framework.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_framework.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bhildebr <bhildebr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/07 18:13:12 by bhildebr          #+#    #+#             */
+/*   Updated: 2024/02/13 15:43:21 by bhildebr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_FRAMEWORK_H
+# define FT_FRAMEWORK_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# include "new.h"
+# include "delete.h"
+# include "allocate.h"
+# include "deallocate.h"
+
+# include "trillian.h"
+# include "any.h"
+
+# include "assert.h"
+# include "debug.h"
+# include "error.h"
+# include "print.h"
+
+#endif
